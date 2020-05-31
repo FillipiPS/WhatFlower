24ad219216d0b0a60fa34cabbcddb8f18f7ec59a
FillipiPS <fillipi_ps@outlook.com>
2020-05-31 02:45:01 -0300

    Getting Classifications From Model

--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
@@ -24,8 +26,30 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let userPickedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
             imageView.image = userPickedImage
+            guard let convertedCIImage = CIImage(image: userPickedImage) else {
+                fatalError("Cannot convert UIImage to CIImage.")
+            }
+            detect(flowerImage: convertedCIImage)
         }
         imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(flowerImage: CIImage) {
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {
+            fatalError("Loading CoreML Model Faild.")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            let classification = request.results?.first as? VNClassificationObservation
+            self.navigationItem.title = classification?.identifier.capitalized
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: flowerImage)
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
     }
     
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
